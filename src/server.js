98dc456fbd5994e8e40fa54242adf18b689c687e
Igor602437 <a505741747@gmail.com>
2025-10-12 23:28:10 +0300

    puth-rout

--- src/server.js
+++ src/server.js
@@ -1,47 +1,65 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
+// import pino from 'pino-http';
 import 'dotenv/config';
-import { connectMongoDB } from './db/connectMongoDB';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { Student } from './models/student.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
 
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
+// app.use(
+//   pino({
+//     level: 'info',
+//     transport: {
+//       target: 'pino-pretty',
+//       options: {
+//         colorize: true,
+//         translateTime: 'HH:MM:ss',
+//         ignore: 'pid,hostname',
+//         messageFormat:
+//           '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+//         hideObject: true,
+//       },
+//     },
+//   }),
+// );
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString()}`);
+//   next();
+// });
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
+// app.get('/notes', (req, res) => {
+//   res.status(200).json({ message: 'Retrieved all notes' });
+// });
+
+// app.get('/notes/:noteId', (req, res) => {
+//   const { noteId } = req.params;
+//   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+// });
 
 app.get('/test-error', () => {
   throw new Error('Simulated server error');
+});
+
+app.get('/students', async (req, res) => {
+  const students = await Student.find();
+  res.status(200).json(students);
+});
+
+app.get('/students/:studentId', async (req, res) => {
+  const { studentId } = req.params;
+  const student = await Student.findById(studentId);
+
+  if (!student) {
+    return res.status(404).json({ message: 'Student not found' });
+  }
+
+  res.status(200).json(student);
 });
 
 app.use((req, res) => {
@@ -58,9 +76,14 @@ app.use((req, res) => {
 //   });
 // });
 app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
   res.status(500).json({
-    message: err.message,
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
   });
 });
 
